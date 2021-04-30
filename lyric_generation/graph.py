2763e80b8e30ac28474b2e2cdbe442336b58f1d8
Thomas Dhome-Casanova <tmd4@princeton.edu>
2021-04-30 02:55:21 -0400

    applied prior and edited calc for peak performance

--- lyric_generation/graph.py
+++ lyric_generation/graph.py
@@ -3,6 +3,7 @@ import pandas as pd
 import contractions
 import re
 import numpy as np
+import json
 
 processed_song_df = pd.read_csv('temp/processed_song_df.csv')
 processed_song_df.drop('Unnamed: 0', axis=1, inplace=True)
@@ -60,6 +61,15 @@ for word, next_words_dict in bigram_words_counts.items():
         bigram_words_counts[word][nextword] = next_words_dict.get(nextword,0) + .3
 
 # apply prior
+json_file = open('./temp/lda_words_categ1.json')
+prior_input = json.load(json_file) # list of ['word', probability_float]
+prior_word_set = set([member[0] for member in prior_input])
+matching_words_set = prior_word_set&all_words
+print('applying prior of ' + str(len(matching_words_set)) + ' words')
+for word, next_words_dict in bigram_words_counts.items():
+    for nextword in matching_words_set:
+        bigram_words_counts[word][nextword] = next_words_dict.get(nextword,0) * 1.4
+
 
 
 # %% --- construct graph --------------------------------------------
@@ -90,8 +100,8 @@ for word in all_words:
     if(word in no_out_connections): continue
 
     node_key = "0_" + word
-    weight = log(1/orig_word_dict[word])+ 10
-    
+    # weight = log(1/orig_word_dict[word])+ 10
+    weight = 1/orig_word_dict[word]
     edge_tuple = ('', node_key, weight)
     edge_tuple_list.append(edge_tuple)
     
@@ -108,8 +118,8 @@ for i in tqdm(range(n_words-1)):
         for right_word in all_words:
             if(right_word in no_out_connections): continue
             right_node_key = str(i+1) + "_" + right_word
-            weight = log(1/orig_word_dict[right_word]) + 10
-            
+            # weight = log(1/orig_word_dict[right_word]) + 10
+            weight = 1/orig_word_dict[right_word]
             edge_tuple = (left_node_key, right_node_key, weight)
             edge_tuple_list.append(edge_tuple)
         
@@ -119,8 +129,8 @@ for left_word in all_words:
 
     orig_word_dict = bigram_words_counts.get(left_word)
     left_node_key = str(n_words-1) + "_" + left_word
-    weight = log(1/orig_word_dict['endofline'])+ 10
-
+    # weight = log(1/orig_word_dict['endofline'])+ 10
+    weight = 1/orig_word_dict['endofline']
     edge_tuple = (left_node_key, 'endofline', weight)
     edge_tuple_list.append(edge_tuple)
 
@@ -134,7 +144,8 @@ from pqdict import pqdict
 def get_weight(g, path):
     weight = 1
     for i in range(len(path)-1):
-        edge_weight = g[path[i], path[i+1]]
+        # edge_weight = g[path[i], path[i+1]]
+        edge_weight = g.es[g.get_eid(path[i], path[i+1], error=False)]['weight']
         weight = weight*edge_weight
     return weight
 
@@ -144,6 +155,7 @@ def get_string(g, path):
         key = g.vs['name'][path_member]
         word = key[2:]
         word = re.sub(r"\u2005", " ", word) #forgot to take these out first
+        word = re.sub(r"\u205f", " ", word) #forgot to take these out first
         if(key!=''): 
             string_arr =string_arr + [word]
     string_arr[0] = string_arr[0].capitalize()
@@ -162,7 +174,7 @@ for i in tqdm(range(500000)):
 minpq = pqdict(minpq_dict)
 top_path_ids = list(minpq.popkeys())[:10]
 top_strings = [get_string(g, all_paths[index]) for index in top_path_ids]
-print('Best paths:')
+print('Best random walk paths:')
 print(top_strings)
 
 # %% --- copying is super fast so let's see how fast dropping nodes is ---------------------------------------------------------
@@ -173,11 +185,12 @@ for i in range(n_words):
     begining = str(i)+'_'
     nodes_in_col = [v.index for v in d.vs if v['name'][:2]==begining]
     # https://www.geeksforgeeks.org/randomly-select-n-elements-from-list-in-python/
-    count_dropping = int(0.05*len(nodes_in_col))
+    count_dropping = int(0.01*len(nodes_in_col))
     nodes_to_drop = random.sample(nodes_in_col, count_dropping)
     d.delete_vertices(nodes_to_drop)
 path = d.get_shortest_paths('', to='endofline', weights='weight', mode='out', output='vpath')[0]
-get_string(g, path)
 
+path_string = get_string(g, path)
+print(path_string)
 
 
